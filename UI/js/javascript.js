dfa3a9fba8a2e96e4d32355ea47c0d578aa91554
Kiganda Ivan <kigandaivan32@gmail.com>
2018-10-15 12:05:24 +0300

    Adding validation to user login [Fixes #161213225]

--- UI/js/javascript.js
+++ UI/js/javascript.js
@@ -1,7 +1,30 @@
 var login = document.getElementById('login');
+var wrongUsername = document.getElementById('wrong-username');
+var wrongPassword = document.getElementById('wrong-password');
+
 
 login.addEventListener('submit', function(e){
     e.preventDefault();
+
+    console.log(e.target.username.value);
+    console.log(e.target.password.value);
+
+    if (e.target.username.value == ''){
+        wrongUsername.style.display = 'block';
+        wrongUsername.innerText = 'Provide Username';
+    }else if (e.target.username.value != 'admin' || e.target.username.value != 'attendant'){
+        wrongUsername.style.display = 'block';
+        wrongUsername.innerText = 'wrong username';
+    }
+
+    if (e.target.password.value == ''){
+        wrongPassword.style.display = 'block';
+        wrongPassword.innerText = 'Provide Password';
+    }else if (e.target.password.value != 'admin' || e.target.password.value != 'attendant'){
+        wrongPassword.style.display = 'block';
+        wrongPassword.innerText = 'wrong password';
+    }
+
     if (e.target.username.value == 'admin' && e.target.password.value == 'admin'){
         e.target.username.value = '';
         e.target.username.value = '';
