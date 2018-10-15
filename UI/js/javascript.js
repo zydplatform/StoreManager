dc28c31e878fecd4c00378a8241f114d4fa93890
Kiganda Ivan <kigandaivan32@gmail.com>
2018-10-15 12:30:18 +0300

    fixing login validation error2

--- UI/js/javascript.js
+++ UI/js/javascript.js
@@ -7,12 +7,8 @@ login.addEventListener('submit', function(e){
     e.preventDefault();
 
     if (e.target.username.value == 'admin' && e.target.password.value == 'admin'){
-        e.target.username.value = '';
-        e.target.username.value = '';
-        window.location.href = 'admin/all_sales.html'
+        window.location.href = 'admin/all_sales.html';
     } else if (e.target.username.value == 'attendant' && e.target.password.value == 'attendant'){
-        e.target.username.value = '';
-        e.target.username.value = '';
         window.location.href = 'attendant/sell_product.html';
     }
 
