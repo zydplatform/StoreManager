907ac7e7ace89f28f6036a5cf873f6d7a732bf6f
Kiganda Ivan <kigandaivan32@gmail.com>
2018-10-25 08:19:30 +0300

    ch-updated run module that starts the whole application

--- app/run.py
+++ app/run.py
@@ -1,12 +1,15 @@
-from flask import Flask,jsonify,json
+import os
+from flask import json,jsonify,app,Flask
 
 app = Flask(__name__)
 
 app.config['DEBUG'] =True
 
-@app.route('/',methods=['GET'])
-def index():
-    return jsonify({'App':'store manager'})
+# @app.route('/',methods=['GET'])
+# def index():
+#     return jsonify({'App':'store manager'})
 
-if __name__ == "__main__":
-    app.run(port=5000)
+
+# if __name__ == '__main__':
+#     app.run(port=5000)
+
